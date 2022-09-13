fb96242018e1efb36a352ef370b032f12cdcd3ec
nith2001 <nith2001@uw.edu>
2022-09-13 14:10:06 -0700

    Start of home page done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
-import { Typography } from '@mui/material';
+import { Typography, IconButton } from '@mui/material';
+import Logo from '../../assets/HCPLogoText.png'
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import './Home.css';
 
 function Home(props) {
@@ -10,7 +12,22 @@ function Home(props) {
     return (
         <div className='home-background'>
             <div className='home-container'>
-                <Typography variant="h2" color="primary"> Home </Typography>
+                <img src={Logo} alt="logo" id="home-logo"/>
+                <Typography id="motto" component="h2" variant="h4" color="primary"> Let's Git Good! </Typography>
+                <IconButton
+                    color="primary"
+                    href="#home-body"
+                    id="home-scroll-button"
+                >
+                    <ArrowCircleDownIcon sx={{ fontSize: 50 }} />
+                </IconButton>
+                <div id='home-body'>
+                    <Typography id="objective" component="h4" variant="h5" color="primary">
+                        Husky Coding Project’s objective is to break the circular reasoning of
+                        <span id="purple"> “needing experience to get experience” </span>that prevents hundreds of students
+                        from landing their first software internship or job.
+                    </Typography>
+                </div>
             </div>
         </div>
     );
