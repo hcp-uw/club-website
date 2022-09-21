a65517d3763a4f1534ae58e89433030e97f2d1a0
kevkevin999 <94778542+kevkevin999@users.noreply.github.com>
2022-09-20 20:34:00 -0700

    embedded Google Form into join page

--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -11,6 +11,16 @@ function Join(props) {
         <div className='join-background'>
             <div className='join-container'>
                 <Typography variant="h2" color="primary"> Join </Typography>
+                <div className='google-form-embed'>
+                    <iframe
+                        title="club sign up google form"
+                        src="https://docs.google.com/forms/d/e/1FAIpQLSc5wyLuacKqZpezaffwB8jZZVz9yBo83tvA-U_vsRMiPbGslA/viewform?embedded=true"
+                        width="640" height="1800" 
+                        frameborder="0" marginheight="0" marginwidth="0"
+                    >
+                    Loading…
+                    </iframe>
+                </div>
             </div>
         </div>
     );
