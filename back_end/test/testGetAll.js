da6a985554973cc9178c20e960b35bc5048e4c64
Benjamin Carney <47190012+carneyb3592@users.noreply.github.com>
2022-10-09 21:21:29 -0700

    Backend -  Get All Endpoints (#10)
    * new backend, set up file structures
    
    * added temp files for place holders
    
    * Created some simple API (still a work in progress)
    
    * removed duplicate database in end_points
    
    * added tests
    
    * Added testing framework
    
    * Got retreiving data working.
    
    * Finished User Tests and Endpoints
    
    * Simplified End Points
    
    * Added Get All Endpoints
    
    * Adding missing files
    
    Co-authored-by: wu-chinese <merkurieffr@gmail.com>

---
+++ back_end/test/testGetAll.js
@@ -0,0 +1,56 @@
+import { getLeads, getEvents, getProjects } from "../api/end_points.js";
+import {assert} from 'chai';
+
+describe("Testing Get all Functions.", () => {
+  it('Get All Officers',async () => {
+    let rsp = await getLeads("Test/Club_Leads");
+    let expected = 
+    {
+      Billy: {
+        Active: true,
+        Class_Standing: 'Senior',
+        Date_Joined: '6/30/2021',
+        Date_Left: 'NA',
+        Email: 'something@gmail.com',
+        Image: 'NA',
+        Role: 'Co-Chair',
+        Team: 'Project1'
+      }
+    };
+    assert.deepEqual(rsp, expected);
+  })
+
+  it('Get All Events', async () => {
+    let rsp = await getEvents("Test/Events");
+    let expected = 
+    {
+      Event1: {
+        Attendees: 69,
+        Date: '6/12/2007',
+        Description: 'This is a dope event',
+        Location: 'UW',
+        Sponsor: 'Google'
+      }
+    };
+    assert.deepEqual(rsp, expected);
+  })
+
+  it('Get All Projects', async () => {
+    let rsp = await getProjects("Test/Projects");
+    let expected = 
+    {
+      Project1: {
+        Category: 'Web_Project',
+        Completed: false,
+        Description: 'testdesc',
+        End_Date: '',
+        Git_Link: 'link',
+        Image: 'Image',
+        Members: 'Billy',
+        PM: 'Billy',
+        Start_Date: '1/1/2001'
+      }
+    };
+    assert.deepEqual(rsp, expected);
+  })
+})
---
+++ back_end/utils/index.js
@@ -0,0 +1,29 @@
+//  connect to the firebase thing here brother
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// I think we need to import the data base that we are actually going to use
+
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDg_d3B6Yplw_M2qMoZQ6Jtymd_dW5k79Q",
+  authDomain: "husky-coding-project.firebaseapp.com",
+  databaseURL: "https://husky-coding-project-default-rtdb.firebaseio.com",
+  projectId: "husky-coding-project",
+  storageBucket: "husky-coding-project.appspot.com",
+  messagingSenderId: "123708771040",
+  appId: "1:123708771040:web:2e76f3762cef2eba669e85",
+  measurementId: "G-NDX314ZCVW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
