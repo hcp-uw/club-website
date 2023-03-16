edb872d72a4b04d91823c56fe39d70204ce725d1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-16 12:46:29 -0700

    Connect with ChakraProvider

--- hcp-website/pages/_app.tsx
+++ hcp-website/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    
-      <Component {...pageProps} />
-    
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
   )
 }
