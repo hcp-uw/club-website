bb3b4bafa4a74f55cb09f17ec37eca1cbf3b2140
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-16 13:30:51 -0700

    Create Layout

---
+++ hcp-website/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { VStack } from "@chakra-ui/react";
+import type { ReactElement } from "react";
+
+import Header from "../components/Header";
+// import Footer from "../components/Footer";
+
+export default function Layout({ children }: { children: ReactElement }) {
+    return (
+        <VStack spacing={10} align="right" marginY={5}>
+            <Header />
+            {children}
+            {/*<Footer />*/}
+        </VStack>
+    );
+}
--- hcp-website/pages/index.tsx
+++ hcp-website/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,6 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Header />
             <Image
                 src="/output-onlinegiftools.gif"
                 width={100}
