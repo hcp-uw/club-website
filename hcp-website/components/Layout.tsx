9969c63cd1fcde2c8677ec8ae24c5c40893856f5
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-16 13:42:24 -0700

    Create the base of Footer component

--- hcp-website/components/Layout.tsx
+++ hcp-website/components/Layout.tsx
@@ -2,14 +2,14 @@ import { VStack } from "@chakra-ui/react";
 import type { ReactElement } from "react";
 
 import Header from "../components/Header";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 export default function Layout({ children }: { children: ReactElement }) {
     return (
         <VStack spacing={10} align="right" marginY={5}>
             <Header />
             {children}
-            {/*<Footer />*/}
+            <Footer />
         </VStack>
     );
 }
