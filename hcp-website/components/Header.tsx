d573fa3c1d58251d8f27eba8bbf5c3f2fd8f96fa
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-16 13:17:19 -0700

    Format Header.tsx

--- hcp-website/components/Header.tsx
+++ hcp-website/components/Header.tsx
@@ -1,4 +1,12 @@
-import {Flex, Spacer, Image, LinkBox, Link, Button, LinkOverlay} from "@chakra-ui/react";
+import {
+    Flex,
+    Spacer,
+    Image,
+    LinkBox,
+    Link,
+    Button,
+    LinkOverlay,
+} from "@chakra-ui/react";
 
 export default function Header() {
     return (
@@ -6,14 +14,22 @@ export default function Header() {
             <Spacer />
             <LinkBox>
                 <LinkOverlay href="/">
-                <Image width="80px" src="/HCPLogoText.png" alt="HCP Logo" />
+                    <Image width="80px" src="/HCPLogoText.png" alt="HCP Logo" />
                 </LinkOverlay>
             </LinkBox>
             <Spacer />
-            <Link margin={5} href="/home">Home</Link>
-            <Link margin={5} href="/aboutus">About Us</Link>
-            <Link margin={5} href="/projects">Projects</Link>
-            <Link margin={5} href="/events">Events</Link>
+            <Link margin={5} href="/home">
+                Home
+            </Link>
+            <Link margin={5} href="/aboutus">
+                About Us
+            </Link>
+            <Link margin={5} href="/projects">
+                Projects
+            </Link>
+            <Link margin={5} href="/events">
+                Events
+            </Link>
             <Button>Join Us</Button>
             <Spacer />
         </Flex>
