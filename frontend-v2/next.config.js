e8972410f23f7286685a9d91067ffc614e6ed12d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-26 02:18:49 -0700

    Some tweaks

--- frontend-v2/next.config.js
+++ frontend-v2/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+    // for GitHub Pages
+    assetPrefix: isProd ? '/club-website/' : '',
+    images: {
+        unoptimized: true,
+    },
+
     reactStrictMode: true,
     webpack: (config) => {
         config.resolve.alias["@"] = path.resolve(__dirname);
