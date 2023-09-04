2e16046244ec348b4619b801885fd1a7c0f5555e
Rasmus <rasmus.makiniemi@gmail.com>
2023-09-04 16:26:58 -0700

    Created the loop graphic, and finalized the about page by creating formatting functions for the lead names and roles. Also added inline styling.

--- frontend-v2/pages/about.tsx
+++ frontend-v2/pages/about.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import styles from '@/styles/About.module.css'
 import {
   Center,
   VStack,
@@ -8,6 +7,7 @@ import {
   Text,
   IconButton,
   Spacer,
+  Image,
 } from "@chakra-ui/react";
 import { getPeople } from "utils/api";
 import { IPeopleInfo } from "utils/parsers";
@@ -27,6 +27,20 @@ export default function About() {
     getPeople(getData);
   }, []);
 
+  function formatName(name: string) {
+    // example Rasmus Makiniemi
+    const lastInitial = name.split(' ')[1][0] + '.'
+    return name.split(' ')[0] + ' ' + lastInitial;
+  }
+
+  function formatRole(role: string) {
+    // example Onboarding/Education
+    if (role.split('/').length != 1) {
+      return role.split('/')[0] + " & " + role.split('/')[1];
+    }
+    else return role;
+  }
+
   const generateAvatars = () => {
     return (
       <>
@@ -35,8 +49,9 @@ export default function About() {
           {people.map((i) => {
             return(
               <Flex flexDirection='column' alignItems='center' margin='20px' width='15%'>
-                <Box width='150px' height='150px' background='white' borderRadius='50%' marginBottom='30px'></Box>
-                <Text fontSize='2xl' color='white' overflowWrap='anywhere' textAlign='center'>{i.name}</Text>
+                <Box width='150px' height='150px' backgroundImage={i.image} backgroundSize='100%' backgroundPosition='center' borderRadius='50%' marginBottom='30px'></Box>
+                <Text fontSize='2xl' color='white' overflowWrap='anywhere' textAlign='center' width='90%'>{formatName(i.name)}</Text>
+                <Text fontSize='large' color='#FD72F8' overflowWrap='anywhere' textAlign='center'>{formatRole(i.role)}</Text>
               </Flex>
             )
           })}
@@ -62,7 +77,9 @@ export default function About() {
             This circular logic plagues every generation of programmers.
           </Text>
         </Flex>
-        <img src="" alt="some cool design" height='100px' width='30%'></img>
+        <Box height='100px' width='30%' marginLeft='150px' minWidth='400px'>
+          <Image src="/loop-graphic.png" alt="some cool design" width='500px'/>
+        </Box>
       </Flex>
       <Spacer height='150px'></Spacer>
 
@@ -73,13 +90,15 @@ export default function About() {
           <Text fontSize='2xl'>
             To create a tech internship-like environment that promotes the growth and development
             of our club members. We provide team-based programming project experience and encouraging
-            peer-to-peer learning.
+            <Text as='span' color='#FD72F8'> peer-to-peer learning.</Text>
             <br></br><br></br>
             To provide the next generation of programmers & designers with the tools and experience to
-            succeed in future tech careers.
+            <Text as='span' color='#FD72F8'> succeed in future tech careers.</Text>
           </Text>
         </Flex>
-        <img src="@/public/output-onlinegiftools.gif" alt="some cool design" height='100px' width='30%'></img>
+        <Box height='100px' width='30%' marginLeft='150px' minWidth='400px'>
+          <Image src="/output-onlinegiftools.gif" alt="some cool design" width='200%'/>
+        </Box>
       </Flex>
       <Spacer height='150px'></Spacer>
 
